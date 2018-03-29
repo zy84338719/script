025c81637f12ce9a29e2bd30d7868ba47977f021
MurphyYi <zy84338719@hotmail.com>
2018-03-29 17:59:36 +0800

    fix code wrong

--- github_check.py
+++ github_check.py
@@ -6,17 +6,20 @@ import time
 api = 'https://api.github.com/repos/zy84338719/script'
 web_page = 'https://www.github.com/zy84338719/script'
 
-
 last_update = None
 
-
 while True:
+    # 获取当前api链接得到json文件
     all_info = requests.get(api)
+    # 获得字典
     dict_info = all_info.json()
+    # 得到文件更新时间
     cur_update = dict_info['updated_at']
-    if not last_update:
-        last_update =cur_update
 
-    if last_update <cur_update:
+    if not last_update:
+        last_update = cur_update
+
+    if last_update < cur_update:
         webbrowser.open(web_page)
-    time.sleep(10)
+        last_update = cur_update
+    time.sleep(3600)
